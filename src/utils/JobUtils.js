90d8228e1a9534a6d10a959e43a4375b6eabfbe7
Victor Mello <victorsmello93@gmail.com>
2021-04-18 14:50:20 -0300

    corrigido o bug de arredondamento no cálculo de dias restantes para o projeto

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -11,7 +11,7 @@ module.exports = {
 
         //Converter milissegundos em dias
         const dayInMs = 1000 * 60 * 60 * 24
-        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+        const dayDiff = Math.ceil(timeDiffInMs / dayInMs)
 
         //Restam x dias
         return dayDiff
